09c094c5323d56db5c9800f2c53d46805cd50a46
nisrinaRepositori <athirahnisrina27@gmail.com>
2025-09-19 08:02:17 +0700

    Create page.js

---
+++ app/direktori/page.js
@@ -0,0 +1,11 @@
+// app/direktori/page.js
+import Navbar from "@/components/Navbar";
+import Directory from "@/components/Direktori";
+
+export default function DirectoryPage() {
+  return (
+    <div>
+      <Directory />
+    </div>
+  );
+}
