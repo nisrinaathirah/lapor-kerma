c4bc3572baf1be16ba172930346605c2042496ab
Nisrina Athirah Nursyifa <athirahnisrina27@gmail.com>
2025-09-26 09:29:55 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,18 +3,28 @@ import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer className="bg-white py-8 mt-0">
-      <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-6 text-sm text-[#003366]">
+      <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-6 text-sm text-[#003366] item-start">
         
-        {/* Bagian Kiri */}
-        <div className="space-y-2">
-          <h3 className="font-kadwa text-[18px] font-bold">Kementerian Pendidikan</h3>
-          <p className="font-kadwa text-[18px] font-bold pt-0 leading-tight">Tinggi, Sains, dan Teknologi</p>
-          <p className="text-gray-800 font-sans text-[14px]">Jalan Jend. Sudirman Pintu I Senayan</p>
-          <p className="text-gray-800 font-sans text-[14px]">Jakarta</p>
+      {/* Bagian kiri */}
+      <div className="flex items-start gap-4">
+          {/* Logo */}
+          <img 
+            src="/logo2.png" 
+            alt="Logo Kementerian" 
+            className="w-20 h-20 object-contain" 
+          />
+          
+          {/* Teks */}
+          <div className="space-y-1">
+            <h3 className="font-kadwa text-[18px] font-bold">Kementerian Pendidikan</h3>
+            <p className="font-kadwa text-[18px] font-bold pt-0 leading-tight">Tinggi, Sains, dan Teknologi</p>
+            <p className="text-gray-800 font-sans text-[14px]">Jalan Jend. Sudirman Pintu I Senayan</p>
+            <p className="text-gray-800 font-sans text-[14px]">Jakarta</p>
+          </div>
         </div>
         
         {/* Bagian Tengah */}
-        <div className="space-y-2">
+        <div className="space-y-2 pl-18">
           <h3 className="font-kadwa text-[18px] font-bold">Tautan Penting</h3>
           <ul className="space-y-1 text-gray-600">
             <li><a href="#" className="hover:underline"> Kemdikbud</a></li>
